64addaef0f0f5ebeda27c01fc3468dda79cd85d0
Imesh-IntrinsicTech <imesh@intrinsic-tech.net>
2025-08-15 11:19:50 +0530

    deployment changes

---
+++ techmentor-web/deploy.ps1
@@ -0,0 +1,14 @@
+$BUCKET_NAME = "tech-mentor-bucket"
+$REGION = "us-east-1"
+
+Write-Host "Building React app..."
+npm run build
+
+Write-Host "Removing old files from S3..."
+aws s3 rm "s3://$BUCKET_NAME" --recursive
+
+Write-Host "Uploading new files..."
+aws s3 sync "build/" "s3://$BUCKET_NAME" --delete
+
+Write-Host "Deployment complete!"
+Write-Host "Site URL: http://$BUCKET_NAME.s3-website-$REGION.amazonaws.com"
