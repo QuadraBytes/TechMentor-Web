1c1f2c0da53a52666437d5446514f916ee05f868
morawaliyadda <sithara.morawaliyadda2000@gmail.com>
2025-08-24 00:46:24 +0530

    changes

--- techmentor-web/src/utils/baseUrl.js
+++ techmentor-web/src/utils/baseUrl.js
@@ -1,11 +1,5 @@
-// export const baseUrl = "http://localhost:5001/";
-// export const baseUrl = "http://localhost:5002/";
-// export const baseUrl = "http://localhost:5003/";
-// export const baseUrl =
-//   "https://b0faixbrnf.execute-api.ap-south-1.amazonaws.com/prod";
-
-// export const baseUrl = "http://3.7.71.194:5000/api";
-export const baseUrl = "http://localhost:5000/api";
+export const baseUrl =
+  "https://b0faixbrnf.execute-api.ap-south-1.amazonaws.com/prod";
 
 export const googleClientId =
   "313707990375-7m1a54ud4es1b0bko5u7ohj9id42uhs5.apps.googleusercontent.com";
